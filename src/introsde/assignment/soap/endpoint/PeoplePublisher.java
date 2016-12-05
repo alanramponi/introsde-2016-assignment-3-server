7384c805e3916475d7028063cce0ce678df38976
alanramponi <ramponialan@gmail.com>
2016-12-05 15:51:05 +0100

    Some changes to make it work

--- src/introsde/assignment/soap/endpoint/PeoplePublisher.java
+++ src/introsde/assignment/soap/endpoint/PeoplePublisher.java
@@ -28,14 +28,13 @@ public class PeoplePublisher {
     public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
         String PROTOCOL = "http://";
         String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
-        if (HOSTNAME.equals("127.0.0.1"))
-        {
+        if (HOSTNAME.equals("127.0.0.1")) {
             HOSTNAME = "localhost";
         }
         String PORT = "6902";
         String BASE_URL = "/ws/people";
 
-        if (String.valueOf(System.getenv("PORT")) != "null"){
+        if (String.valueOf(System.getenv("PORT")) != "null") {
             PORT=String.valueOf(System.getenv("PORT"));
         }
         
